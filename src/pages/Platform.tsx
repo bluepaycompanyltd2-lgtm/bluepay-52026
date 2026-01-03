b7b69c48058a2ebcbe2d51928d7730d94c190235
bluepaycompanyltd2-lgtm <bluepaycompanyltd2@gmail.com>
2026-01-03 10:00:25 +0100

    Update Platform.tsx

--- src/pages/Platform.tsx
+++ src/pages/Platform.tsx
@@ -9,11 +9,11 @@ const Platform = () => {
   const navigate = useNavigate();
   
   const handleJoinTelegram = () => {
-    window.open("https://t.me/+mC3KerwdSrY1M2I0", "_blank");
+    window.open("https://t.me/+Hc_0LZvi-JhkYThk", "_blank");
   };
   
   const handleJoinWhatsapp = () => {
-    window.open("https://chat.whatsapp.com/EAZg7u9lnYSHMVQYfwXPvP", "_blank");
+    window.open("https://chat.whatsapp.com/FXjprV4Gmy3EqOCz54ipd9", "_blank");
   };
 
   return (
